2634aed31cc055addf1a01cb840c86326e25013b
Caxvalencia <caxvalencia@gmail.com>
2019-11-12 16:26:23 -0500

    Add providers to test gaming-stage-page

--- src/app/presentation/admin/gaming-stage/gaming-stage.page.spec.ts
+++ src/app/presentation/admin/gaming-stage/gaming-stage.page.spec.ts
@@ -1,5 +1,9 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { SERVICE_PROVIDERS } from '@config/service-providers/service-provider.config';
+import { GetCardListUseCase } from '@core/cards/use-cases/get-card-list.use-case';
+import { GetNicknameUseCase } from '@core/nickname/use-cases/get-nickname.use-case';
+import { SetNicknameUseCase } from '@core/nickname/use-cases/set-nickname.use-case';
 
 import { GamingStagePage } from './gaming-stage.page';
 
@@ -9,10 +13,16 @@ describe('GamingStagePage', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ GamingStagePage ],
+      declarations: [GamingStagePage],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [
+        SERVICE_PROVIDERS,
+        GetNicknameUseCase,
+        SetNicknameUseCase,
+        GetCardListUseCase,
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
