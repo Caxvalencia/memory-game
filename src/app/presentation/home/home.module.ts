a2c413c0e4782c78027bd4f0efabdf8a2124a15e
Caxvalencia <caxvalencia@gmail.com>
2019-11-11 21:47:27 -0500

    Update home page for support nickname use case

--- src/app/presentation/home/home.module.ts
+++ src/app/presentation/home/home.module.ts
@@ -1,8 +1,8 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
 
 import { HomePage } from './home.page';
 
@@ -10,6 +10,7 @@ import { HomePage } from './home.page';
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     IonicModule,
     RouterModule.forChild([
       {
@@ -20,4 +21,4 @@ import { HomePage } from './home.page';
   ],
   declarations: [HomePage]
 })
-export class HomePageModule {}
+export class HomePageModule { }
--- src/app/presentation/home/home.page.ts
+++ src/app/presentation/home/home.page.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { SessionService } from '@shared/services/session.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit, OnDestroy {
 
-  constructor() {}
+  public homeForm: FormGroup;
 
+  private unsubscribe$ = new Subject<void>();
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private sessionService: SessionService
+  ) {
+    this.homeForm = this.formBuilder.group({
+      nickname: [
+        '',
+        [
+          Validators.required,
+          Validators.maxLength(20)
+        ]
+      ],
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+
+  onSubmit(data: any) {
+    this.sessionService.setNickname(data.nickname)
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(response => {
+        console.log(response);
+
+        if (response) {
+          this.router.navigate(['admin']);
+        }
+      });
+  }
+
+  get nickname() {
+    return this.homeForm.get('nickname');
+  }
 }
